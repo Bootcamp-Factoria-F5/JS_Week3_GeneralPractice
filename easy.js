579a5956daf7ff23537e44bd4d2eee5525c5cc13
Andreea <andreea.celmare@gmail.com>
2024-10-18 20:42:57 +0200

    finish exercises October 18 JavaScript

--- easy.js
+++ easy.js
@@ -1,6 +1,6 @@
 //let usuario = {};
 
-// crea una función que reciba un número y retorne el mismo número
+// EJERCICIO 1: crea una función que reciba un número y retorne el mismo número
 
 /* function easy(numero) {
   let variable = "el dato";
@@ -22,7 +22,7 @@ console.log(result); */
 let result = saludar("Amr");
 console.log(result); */
 
-// Escribe una función sumaArray que reciba un array de números
+// EJERCICIO 2: Escribe una función sumaArray que reciba un array de números
 // Retornar la suma de todos los números -> implementar la función con un bucle
 
 /* function sumaArray(numeros) {
@@ -37,7 +37,7 @@ const numberList = [1, 2, 3, 4, 5];
 let result = sumaArray(numberList);
 console.log(result); */
 
-// Retornar la suma de todos los números pares -> implementar la función con un bucle
+// EJERCICIO 3: Retornar la suma de todos los números pares -> implementar la función con un bucle
 
 /* function sumaPares(numeros) {
   let suma = 0; //inicializar la suma
@@ -60,9 +60,9 @@ let resultado = sumaPares(arrayDeNumeros);
 // comprobar el resultado
 console.log(resultado); */
 
-// Escribe una función createUser que reciba un nombre de usuario, una edad
+// EJERCICIO 4: Escribe una función createUser que reciba un nombre de usuario, una edad
 
-function createUser(name, age) {
+/* function createUser(name, age) {
   const user = {
     nombre: name,
     edad: age,
@@ -77,4 +77,60 @@ function createUser(name, age) {
 //CASO  DE PRUEBA
 
 let resultado = createUser("Elena", 20);
-console.log(resultado);
+console.log(resultado); */
+
+// EJERCICIO 5: Escribe una función que reciba una lista de usuarios {nombre, edad}
+// Retornar una lista de los usuarios que sean mayores de edad
+
+/* function filterAdultUsers(users) {
+  const listAdultUsers = [];
+
+  for (const user of users) {
+    if (user.age >= 18) {
+      listAdultUsers.push(user);
+    }
+  }
+  return listAdultUsers;
+}
+
+// CASO DE PRUEBA
+
+const listUsers = [
+  {
+    name: "Pablo",
+    age: 30,
+  },
+  {
+    name: "Paloma",
+    age: 3,
+  },
+  {
+    name: "Kiki",
+    age: 18,
+  },
+];
+
+// Ejecutamos la función
+let result = filterAdultUsers(listUsers);
+
+// Comprobrar el resultado
+console.log(result); */
+
+// VERSION CON FILTRO
+
+function filterAdults(users) {
+  const filteredUsers = users.filter((user) => user.edad >= 18);
+  return filteredUsers;
+}
+
+// CASO PRUEBA
+const userList = [
+  { nombre: "Juan", edad: 25 },
+  { nombre: "Maria", edad: 18 },
+  { nombre: "Pedro", edad: 32 },
+  { nombre: "Luis", edad: 16 },
+  { nombre: "Ana", edad: 28 },
+];
+
+const result = filterAdults(userList);
+console.log(result);
