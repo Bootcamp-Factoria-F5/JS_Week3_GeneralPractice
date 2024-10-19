1424b6199f3eeaf6098bae254eada02336d1335f
Andreea <andreea.celmare@gmail.com>
2024-10-19 12:12:30 +0200

    feat(grover): finish learning Use of Variables

--- grover.js
+++ grover.js
@@ -1,1 +1,29 @@
 // Exercicios para aprender JavaScript, por Grover Vásquez en su curso de Udemy
+
+// USO DE VARIABLES
+/* console.log("Hola a todos");
+console.log(5 + 5);
+
+var nombre = "Grover";
+console.log(nombre);
+console.log(typeof(nombre));
+
+var edad = 34;
+console.log(edad);
+console.log(typeof(edad));
+
+var sueldo = 1900.99;
+console.log(sueldo);
+console.log(typeof(sueldo));
+
+var tieneTrabajo = false;
+console.log(tieneTrabajo);
+console.log(typeof(tieneTrabajo));
+
+var puestoDeTrabajo;
+console.log(puestoDeTrabajo);
+puestoDeTrabajo = null;
+console.log(puestoDeTrabajo); */
+
+// OPERADORES MATEMATICOS
+
