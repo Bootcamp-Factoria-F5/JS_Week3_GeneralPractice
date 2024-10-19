21bebeb23c69a7e7b5ad74cf769666d103d1e57b
Andreea <andreea.celmare@gmail.com>
2024-10-19 12:38:39 +0200

    feat(grover): finish learning about Operators in JS

--- grover.js
+++ grover.js
@@ -25,5 +25,48 @@ console.log(puestoDeTrabajo);
 puestoDeTrabajo = null;
 console.log(puestoDeTrabajo); */
 
-// OPERADORES MATEMATICOS
+// OPERADORES MATEMATICOS +, -, *, /
 
+/* var edadAna, edadMaria, diferenciaEdad;
+var sumaEdades, yearAna, yearMaria, yearActual;
+
+edadAna = 34;
+edadMaria = 28;
+yearActual = 2024;
+
+diferenciaEdad = edadAna - edadMaria;
+sumaEdades = edadAna + edadMaria;
+yearAna = yearActual - edadAna;
+yearMaria = yearActual - edadMaria;
+
+console.log(diferenciaEdad);
+console.log(sumaEdades);
+console.log('Año en que nació Ana ' + yearAna);
+console.log('Año en que nació Maria ' + yearMaria);
+console.log(yearActual * 5);
+console.log(yearActual / 2); */ 
+
+// OPERADORES LÓGICOS, UNARIOS Y DE ASIGNACIÓN
+
+// Lógicos <, >, <=, >=, ==   ----- utilizados para hacer comparaciones
+
+var edadAna, edadMaria, diferenciaEdad;
+edadAna = 34;
+edadMaria = 28;
+
+var mayorAna = !(edadAna == edadMaria);
+console.log(mayorAna);
+
+// Unarios ++ Incremento, -- Decremento
+
+edadAna++;
+console.log(edadAna);
+
+// Asignación +=, -=, *=, /=, %=
+var a = 11;
+var b = 5;
+var c = a % 5;     //Residuo o resto de una división. 5*2 = 10. Me devuelve 1 de 11.
+console.log(c);
+
+a += b;            // a = a + b;
+console.log(a);
