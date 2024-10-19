8ba6bf391baedf7d07c42a4426d19dd5855d297d
Andreea <andreea.celmare@gmail.com>
2024-10-19 18:21:31 +0200

    feat(grover): ends learning about Switch, For, While, Do While

--- grover.js
+++ grover.js
@@ -1,6 +1,6 @@
-// Exercicios para aprender JavaScript, por Grover Vásquez en su curso de Udemy
+// ***** Exercicios para aprender JavaScript, por Grover Vásquez en su curso de Udemy
 
-// USO DE VARIABLES
+//** USO DE VARIABLES
 /* console.log("Hola a todos");
 console.log(5 + 5);
 
@@ -25,7 +25,7 @@ console.log(puestoDeTrabajo);
 puestoDeTrabajo = null;
 console.log(puestoDeTrabajo); */
 
-// OPERADORES MATEMATICOS +, -, *, /
+//** OPERADORES MATEMATICOS +, -, *, /
 
 /* var edadAna, edadMaria, diferenciaEdad;
 var sumaEdades, yearAna, yearMaria, yearActual;
@@ -46,9 +46,9 @@ console.log('Año en que nació Maria ' + yearMaria);
 console.log(yearActual * 5);
 console.log(yearActual / 2); */ 
 
-// OPERADORES LÓGICOS, UNARIOS Y DE ASIGNACIÓN
+//***** OPERADORES LÓGICOS, UNARIOS Y DE ASIGNACIÓN
 
-// Lógicos <, >, <=, >=, ==   ----- utilizados para hacer comparaciones
+//** Lógicos <, >, <=, >=, ==   ----- utilizados para hacer comparaciones
 
 /* var edadAna, edadMaria, diferenciaEdad;
 edadAna = 34;
@@ -57,12 +57,12 @@ edadMaria = 28;
 var mayorAna = !(edadAna == edadMaria);
 console.log(mayorAna);
 
-// Unarios ++ Incremento, -- Decremento
+//** Unarios ++ Incremento, -- Decremento
 
 edadAna++;
 console.log(edadAna);
 
-// Asignación +=, -=, *=, /=, %=
+//** Asignación +=, -=, *=, /=, %=
 var a = 11;
 var b = 5;
 var c = a % 5;     //Residuo o resto de una división. 5*2 = 10. Me devuelve 1 de 11.
@@ -71,9 +71,9 @@ console.log(c);
 a += b;            // a = a + b;
 console.log(a); */
 
-// SENTENCIAS Y ESTRUCTURAS DE CONTROL: if else, if anidados, switch, for, while, do while 
+//** SENTENCIAS Y ESTRUCTURAS DE CONTROL: if else, if anidados, switch, for, while, do while 
 
-// If Else     if (contition) sentencia1 [else sentencia 2]
+//** If Else     if (contition) sentencia1 [else sentencia 2]
 
 /* var nombre = "Grover";
 var esCasado = false;
@@ -84,7 +84,7 @@ if (esCasado == true) {
     console.log(nombre + ' es soltero')
 }  */
 
-// SENTENCIAS LÓGICAS   Not, And, Or
+//** SENTENCIAS LÓGICAS   Not, And, Or
 
 /* var nombre = "Grover";
 var edad = 63;
@@ -104,7 +104,54 @@ console.log(nombre + ' es un adulto ')
     console.log(nombre + ' es un anciano')
 } */ 
 
-// SENTENCIA SWITCH 
+//** SENTENCIA SWITCH 
+
+/* var mes = 6;
+switch(mes) {
+    case 1:
+        console.log('Enero');
+        break
+    case 2:
+        console.log('Febrero');
+        break
+    case 3:
+        console.log('Marzo');
+        break
+    case 4:
+        console.log('Abril');
+        break
+    case 5:
+        console.log('Mayo');
+        break
+    default:
+        console.log('Mes no encontrado');
+} */ 
+
+//** SENTENCIA FOR
+
+// for ([expresion-inicial]; [condición], [expresión-final])sentencia;
+
+/* for (var i=5; i <= 25; i+=5){
+    console.log(i);
+}
 
 
+//** ESTRUCTURAS WHILE, DO WHILE
+
+var i = 1;
+while (i<5) {
+   console.log(i++); 
+} */
+
+//** SENTENCIA DO WHILE
+
+/* var i =1;
+do{
+    console.log(i);
+    i++;
+}while(i <= 10);
+*/
+
+
+// ***** FUNCIONES
 
