daac77750bf26f040d832ecc849c058e3b19f38b
Andreea <andreea.celmare@gmail.com>
2024-10-22 11:58:47 +0200

    build: create objetos.js

---
+++ objetos.js
@@ -0,0 +1,20 @@
+const myCar = {
+    marca: "Seat",
+    modelo: "Ibiza",
+
+arrancar() {
+console.log("el coche " + this.marca +  " ha arrancado");
+} ,
+};
+
+const PaolasCar = {
+    marca: "Marca2",
+    modelo: "modelo2",
+
+    arrancar() {
+        console.log("el coche ha arrancado");
+    }
+}
+
+console.log(myCar.marca);
+
